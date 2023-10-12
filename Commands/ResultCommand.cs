9b7f174bb2c95527d648f78a85af524cf4bb7d1b
LukaszMichalski03 <michalski469@gmail.com>
2023-10-12 22:08:12 +0200

    first commit

--- Commands/ResultCommand.cs
+++ Commands/ResultCommand.cs
@@ -15,8 +15,10 @@ namespace TicTacToeTest.Commands
         }
 
         public override void Execute(object? parameter)
-        {            
-            if (
+        {     
+            if(IfDraw()) _boardViewModel.ResetBoardCommand.Execute(null);
+            else if (
+                !IfDraw()&&(
                 (_boardElements[0].Symbol == _boardElements[3].Symbol && _boardElements[0].Symbol == _boardElements[6].Symbol) && _boardElements[0].Symbol != "" ||
                 (_boardElements[1].Symbol == _boardElements[4].Symbol && _boardElements[1].Symbol == _boardElements[7].Symbol) && _boardElements[1].Symbol != "" ||
                 (_boardElements[2].Symbol == _boardElements[5].Symbol && _boardElements[2].Symbol == _boardElements[8].Symbol) && _boardElements[2].Symbol != "" ||
@@ -25,22 +27,33 @@ namespace TicTacToeTest.Commands
                 (_boardElements[6].Symbol == _boardElements[7].Symbol && _boardElements[6].Symbol == _boardElements[8].Symbol) && _boardElements[6].Symbol != "" ||
                 (_boardElements[0].Symbol == _boardElements[4].Symbol && _boardElements[0].Symbol == _boardElements[8].Symbol) && _boardElements[0].Symbol != "" ||
                 (_boardElements[2].Symbol == _boardElements[4].Symbol && _boardElements[2].Symbol == _boardElements[6].Symbol) && _boardElements[2].Symbol != ""
-                )
-            {
-                int X =0, O =0;
-                foreach ( var boardElement in _boardElements )
+                ))
                 {
-                    if (boardElement.Symbol == "X") X++;
-                    if (boardElement.Symbol == "O") O++;
+                    int X =0, O =0;
+                    foreach ( var boardElement in _boardElements )
+                    {
+                        if (boardElement.Symbol == "X") X++;
+                        if (boardElement.Symbol == "O") O++;
                     
-                }                
+                    }                
                 
-                if (X>O && X!=0) _boardViewModel.Player1_Wins++;
-                else _boardViewModel.Player2_Wins++;
+                    if (X>O && X!=0) _boardViewModel.Player1_Wins++;
+                    else _boardViewModel.Player2_Wins++;
                 
-                _boardViewModel.ResetBoardCommand.Execute(null);
-            }
+                    _boardViewModel.ResetBoardCommand.Execute(null);
+                }           
             
+        }
+        public bool IfDraw()
+        {
+            int i = 0;
+            foreach(var Element in _boardElements )
+            {
+                if (Element.Symbol == "X" || Element.Symbol == "O") i++;                
+            }
+            if (i >=9) return true;
+            return false;
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# WPF-TicTacToe" 
+"# WPF-TicTacToe" 
